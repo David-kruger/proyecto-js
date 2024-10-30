9cbd5d34c82a41f743decb2a6826476e6d24bcfd
David <52804242+DavidOleas26@users.noreply.github.com>
2024-10-29 22:01:53 -0500

    agregar estilos en las pantallas de home y allFlats, agregar diseños de las tablas, diseños de filtros y ordenamiento, boton de favoritos

--- public/js/home.js
+++ public/js/home.js
@@ -26,22 +26,30 @@ const fillTable = () =>
             flatsTable.forEach(flat => {
                 const tr = document.createElement('tr');
                 tr.innerHTML = 
-                '<td>' + flat.city + '</td>' +
-                '<td>' + flat.streetName + '</td>' +
-                '<td>' + flat.streetNumber + '</td>' +
-                '<td>' + flat.ac + '</td>' +
-                '<td>' + flat.areaSize + '</td>' +
-                '<td>' + flat.rentPrice + '</td>' +
-                '<td>' + flat.dateAvailable + '</td>'+ 
-                '<td>' + flat.yearBuilt + '</td>'; 
+                '<td class="py-2 px-3 text-left">' + flat.city +'</td>' +
+                '<td class="py-2 px-3 text-left ">' + flat.streetName + '</td>' +
+                '<td class="py-2 px-3 text-right">' + flat.streetNumber + '</td>' +
+                '<td class="py-2 px-3 text-left">' + flat.ac + '</td>' +
+                '<td class="py-2 px-3 text-right">' + flat.areaSize + '</td>' +
+                '<td class="py-2 px-3 text-right">' + flat.rentPrice + '</td>' +
+                '<td class="py-2 px-3 text-center">' + flat.dateAvailable + '</td>'+ 
+                '<td class="py-2 px-3 text-center whitespace-nowrap">' + flat.yearBuilt + '</td>'; 
                 
                 const tdAdd = document.createElement('td');
+                tdAdd.className = "py-2 px-3 text-center";
+
                 const button = document.createElement('button');
+                button.innerHTML= '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 fill-[#EFB810]">'+
+                                    '<path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />'+
+                                    '</svg>'
+
                 button.onclick = (event) => RemoveFavorite(event,flat.id);
-                button.innerText = 'Remove';
+
+                // button.innerText = 'Remove';
                 tdAdd.appendChild(button);
                 tr.appendChild(tdAdd)
                 
+                tr.className = "border-y border-gray-300 py-2";
                 tbody.appendChild(tr)
             });                   
         }
